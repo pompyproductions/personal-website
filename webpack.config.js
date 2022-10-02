045a63ded9532bc1d7bd3ff9c3aee8f7ced1c9d1
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-03 00:20:18 +0100

    Basic react setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// the HTML plugin will inject the code you create into a "template" file
+
+module.exports = {
+    entry: "./src/react/index.jsx", // this is the "master" file
+    output: { // this is where the final bundle goes
+        path: path.join(__dirname, "/dist"),
+        filename: "main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "/src/react/template.html"
+        })
+    ]
+}
