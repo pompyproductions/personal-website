b635a04485d0e4b7b348b0dc3def35514d79736b
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-07 19:06:03 +0100

    Working background canvas

--- src/react/components/CanvasBackground.jsx
+++ src/react/components/CanvasBackground.jsx
@@ -1,11 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const CanvasBackground = (props) => {
 
-    return <canvas />
-}
+    const canvasRef = useRef();
+    const [bgStyle, setBgStyle] = useState({
+        transition: "background-color .5s",
+    });
+    const colors = [
+        "hsl(160deg 85% 60%)",
+        "hsl(80deg 85% 60%)",
+        "hsl(10deg 85% 60%)"
+    ]
 
-CanvasBackground.defaultProps = {
-};
+    // set stuff on mount
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvasRef.current.width = window.innerWidth;
+        canvasRef.current.height = window.innerHeight;
+        const ctx = canvasRef.current.getContext("2d");
+    }, [])
+
+    const draw = (ctx, color) => {
+        ctx.fillStyle = color;
+        ctx.strokeStyle = "white";
+        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        ctx.beginPath();
+
+        for (let i = 0; i < 50; i++) {
+            const radius = 10 + 100 * Math.random();
+            const pos = [
+                canvasRef.current.width * Math.random(), 
+                canvasRef.current.height * Math.random()
+            ];
+            ctx.moveTo(pos[0] + radius, pos[1]);
+            ctx.arc(
+                pos[0], pos[1], radius,
+                0, 2 * Math.PI
+            )
+        }
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    const handle = () => {
+        randomizeCanvas(canvasRef.current.getContext("2d"));
+    }
+
+    function randomizeCanvas(ctx) {
+        const color = `hsl(${Math.floor(360 * Math.random())}deg 85% 60%)`;
+        setBgStyle({
+            ...bgStyle,
+            backgroundColor: color
+        });
+        draw(ctx, color);
+    }
+
+    return (
+        <div className="canvas-container">
+            <canvas style={bgStyle} ref={canvasRef}/>
+            <button onClick={handle} className="restartSvg">Press Me</button>
+        </div>
+    )
+}
 
 export default CanvasBackground;
--- src/react/pages/Wip.jsx
+++ src/react/pages/Wip.jsx
@@ -1,44 +1,8 @@
 import React, { Component } from "react";
 import AnimText from "../components/AnimText.jsx";
+
 import CanvasBackground from "../components/CanvasBackground.jsx";
-
-// import 
-
-const PageGreeting = props => {
-    const greetings = {
-        // template: generic, morning, generic, afternoon, generic
-        "tr": ["Selam", "Günaydın", "Selam", "Tünaydın", "İyi akşamlar"],
-        "en": ["Hello", "Good morning", "Hello", "Good afternoon", "Good evening"],
-        "fr": ["Salut", "Bonjour", "Salut", "Salut", "Bonsoir"],
-        "es": ["¡Hola", "¡Buenos días", "¡Buenos días", "¡Buenas tardes", "¡Buenas noches"],
-        "it": ["Ciao", "Buongiorno", "Ciao", "Ciao", "Buonasera"]
-    }
-    let hour = new Date();
-    hour = hour.getHours();
-    
-    let greetingIndex = 0;
-    for (let breakpoint of [6, 11, 16, 20]) {
-        if (hour > breakpoint) {
-        greetingIndex++
-        } else {break};
-    }
-    
-    const [message, setMessage] = React.useState(greetings["tr"][greetingIndex]);
-    const [currentLang, setLang] = React.useState("tr");    
-   
-    const nextGreeting = () => {
-        const langs = Object.keys(greetings);
-        const nextLang = langs[(langs.indexOf(currentLang) + 1) % langs.length];
-        setLang(nextLang);
-        setMessage(greetings[nextLang][greetingIndex]);
-    }
-    
-    return (
-        <span onClick={nextGreeting} className="typing">
-            <AnimText content={`${message}!`} tag="h1" delay={30} />
-        </span>
-      )
-  }
+import PageGreeting from "../components/PageGreeting.jsx"
 
 const Wip = () => {
     return (
