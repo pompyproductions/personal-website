18e3a715272edbfc0457f1c5e962536b20806ae2
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-08 12:00:44 +0100

    Add href to links

--- src/react/pages/Wip.jsx
+++ src/react/pages/Wip.jsx
@@ -13,9 +13,9 @@ const Wip = () => {
                 <p>The website is under construction.</p>
                 <p>In the meantime, you can check out the following links:</p>
                 <li>
-                    <ul>My <a>GitHub profile</a></ul>
-                    <ul>My <a>LinkedIn profile</a></ul>
-                    <ul>My <a>CodePen profile</a></ul>
+                    <ul>My <a href="https://github.com/pompyproductions" target="_blank">GitHub profile</a></ul>
+                    <ul>My <a href="https://www.linkedin.com/in/erengazioglu" target="_blank">LinkedIn profile</a></ul>
+                    <ul>My <a href="https://codepen.io/pompyproductions" target="_blank">CodePen profile</a></ul>
                 </li>
                 <p>Thank you for visiting!{"\n"}— Eren Gazioglu</p>
             </div>
