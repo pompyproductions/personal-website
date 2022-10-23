aa88d25ad3defd6238b20aa1af275139cd118e84
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-23 23:29:26 +0100

    Add knob rotation

--- src/react/components/KnobSelector.jsx
+++ src/react/components/KnobSelector.jsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const KnobSelector = (props) => {
+    const needleRef = useRef();
+    const [rotation, setRotation] = useState(0);
+
+    console.log(props.options);
+    
+    const handleKnobClick = () => {
+        setRotation(prev => {
+            return prev + 1;
+        });
+    }
+    
+    useEffect(() => {
+        needleRef.current.style.transform = `rotate(${rotation * 90 + 45}deg)`;
+        console.log(rotation);
+    }, [rotation])
+
     return (
-        <div class="theme-knob">
+        <div className="theme-knob">
             <p>Theme:</p>
-            <div class="knob-container">
-                <button class="knob-selector">
-                    <span></span>
+            <div className="knob-container">
+                <button className="knob-selector" onClick={handleKnobClick}>
+                    <span ref={needleRef}></span>
                 </button>
                 <p>One</p>
                 <p>Two</p>
