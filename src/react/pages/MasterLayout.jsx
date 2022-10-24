60c9b9ea072f56ee64f970a8d66eb911f610f39a
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-24 02:26:12 +0100

    General restyling

--- src/react/pages/MasterLayout.jsx
+++ src/react/pages/MasterLayout.jsx
@@ -21,6 +21,9 @@ const MasterLayout = () => {
                 <p>I'm Eren Gazioglu, a self-taught front-end developer with a background in architecture.</p>
                 <p>You can find the source code for this webpage following this link. Feel free to copy things or to reach out if you need any clarifications!</p>
             </main>
+            <footer>
+                <p>Status: Currently under development.</p>
+            </footer>
         </>
     )
 }
