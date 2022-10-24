71d7ea37c5095ef0312963e5bacb454172b729d1
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-24 20:36:46 +0100

    Implement FontAwesome icons

--- src/react/components/IconButton.jsx
+++ src/react/components/IconButton.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import RefreshIcon from "../../assets/refresh.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const IconButton = (props) => {
 
     return (
         <div className="icon-button">
             <button onClick={props.onClick}>
-                <props.icon />
+                {/* <props.icon /> */}
+                <FontAwesomeIcon icon={faBars} />
+
             </button>
             <p>{props.content}</p>
         </div>
