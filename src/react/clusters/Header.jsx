65c3bbfbc719be23f6a2b057fc9c1e1134e9223c
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-26 00:41:37 +0100

    Change project structure (clusters folder)

---
+++ src/react/clusters/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import PageLogo from "../components/PageLogo.jsx";
+import KnobSelector from "../components/KnobSelector.jsx";
+import IconButton from "../components/IconButton.jsx";
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+    const toggleHamburger = () => {
+        document.querySelectorAll(".hamburger").forEach(elem => {
+            elem.classList.toggle("active");
+        })
+    }
+
+    return <header>
+        <PageLogo strokeWeight={3} strokeColor="#fcf0f0"/>
+        <KnobSelector />
+        <IconButton icon={faBars} 
+            className="hamburger" 
+            onClick={toggleHamburger}/>
+    </header>
+}
+
+export default Header;
--- src/react/pages/MasterLayout.jsx
+++ src/react/pages/MasterLayout.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 
-import PageLogo from "../components/PageLogo.jsx";
-import PageGreeting from "../components/PageGreeting.jsx"
-import KnobSelector from "../components/KnobSelector.jsx";
-import IconButton from "../components/IconButton.jsx";
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Header from '../clusters/Header.jsx';
 import HamburgerMenu from "../clusters/HamburgerMenu.jsx";
+
+import PageGreeting from "../components/PageGreeting.jsx"
 
 
 const MasterLayout = () => {
-    const toggleHamburger = () => {
-        document.querySelectorAll(".hamburger").forEach(elem => {
-            elem.classList.toggle("active");
-        })
-    }
 
     return (
         <>
             <HamburgerMenu />
-            <header>
-                <PageLogo strokeWeight={3} strokeColor="#fcf0f0"/>
-                <KnobSelector />
-                <IconButton icon={faBars} 
-                    className="hamburger" 
-                    onClick={toggleHamburger}/>
-            </header>
+            <Header />
             <main>
                 <PageGreeting />
                 <p>I'm Eren Gazioglu, a self-taught front-end developer with a background in architecture.</p>
