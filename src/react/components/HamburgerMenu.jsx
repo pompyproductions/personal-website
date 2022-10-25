8b3a4aec58d79ef6e56201daaeea1beaab29eb78
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-25 22:44:13 +0100

    Add border radius

--- src/react/components/HamburgerMenu.jsx
+++ src/react/components/HamburgerMenu.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import IconButton from "./IconButton.jsx";
 
 const HamburgerMenu = () => {
     return <>
