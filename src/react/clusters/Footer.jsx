10df21b248e86da72a8cb597544f38ac395a48e3
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-26 23:48:13 +0100

    Add icons to footer

--- src/react/clusters/Footer.jsx
+++ src/react/clusters/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faCodepen, faBandcamp } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
     return <footer>
-        <p>Status: Currently under development.</p>
+        <ul>
+            <li><a href=""><FontAwesomeIcon icon={faLinkedin}/></a></li>
+            <li><a href=""><FontAwesomeIcon icon={faGithub}/></a></li>
+            <li><a href=""><FontAwesomeIcon icon={faCodepen}/></a></li>
+            <li><a href=""><FontAwesomeIcon icon={faBandcamp}/></a></li>
+        </ul>
     </footer>
 }
 
