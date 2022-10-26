7603249292d8de29f7ec67cfb65aec263448ef47
Eren Gazioglu <pompyproduction@gmail.com>
2022-10-26 23:52:56 +0100

    Add links to footer icons

--- src/react/clusters/Footer.jsx
+++ src/react/clusters/Footer.jsx
@@ -6,10 +6,10 @@ import { faLinkedin, faGithub, faCodepen, faBandcamp } from '@fortawesome/free-b
 const Footer = () => {
     return <footer>
         <ul>
-            <li><a href=""><FontAwesomeIcon icon={faLinkedin}/></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faGithub}/></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faCodepen}/></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faBandcamp}/></a></li>
+            <li><a href="https://www.linkedin.com/in/erengazioglu/" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a></li>
+            <li><a href="https://github.com/pompyproductions" target="_blank"><FontAwesomeIcon icon={faGithub}/></a></li>
+            <li><a href="https://codepen.io/pompyproductions" target="_blank"><FontAwesomeIcon icon={faCodepen}/></a></li>
+            <li><a href="https://granoturco.bandcamp.com/" target="_blank"><FontAwesomeIcon icon={faBandcamp}/></a></li>
         </ul>
     </footer>
 }
