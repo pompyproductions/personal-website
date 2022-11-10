aa6f6480a26eefae64b3659e3fc8edf6d8c3363c
Eren Gazioglu <pompyproduction@gmail.com>
2022-11-10 03:47:48 +0000

    Quick and dirty landing prototype

--- src/react/pages/Landing.jsx
+++ src/react/pages/Landing.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 import PageGreeting from "../components/PageGreeting.jsx";
+import PageLogo from '../components/PageLogo.jsx';
 
 const Landing = () => {
     return (
         <main className='landing'>
             <div>
-                <p>Work in progress.</p>
+                <PageLogo />
+                <PageGreeting />
+            </div>
+            <div>
+                <p>
+                    If you made it here, it means that you followed some sort of link. 
+                </p>
+                <h2>Thank you for your interest!</h2>
+                <p>
+                    This landing page is still under construction, 
+                    but all the information that will be displayed here can be accessed 
+                    by following the links below.
+                </p>
             </div>
         </main>
     )
