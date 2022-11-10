f7abc49cc7fa62a9f00889a62fb74837935fa1e6
Eren Gazioglu <pompyproduction@gmail.com>
2022-11-10 12:57:58 +0000

    Try out styling on landing page

--- src/react/pages/Landing.jsx
+++ src/react/pages/Landing.jsx
@@ -7,19 +7,19 @@ import PageLogo from '../components/PageLogo.jsx';
 const Landing = () => {
     return (
         <main className='landing'>
+            <PageLogo />
             <div>
-                <PageLogo />
                 <PageGreeting />
             </div>
             <div>
                 <p>
-                    If you made it here, it means that you followed some sort of link. 
+                    If you made it here, it means that you probably followed some sort of link. 
                 </p>
                 <h2>Thank you for your interest!</h2>
                 <p>
-                    This landing page is still under construction, 
-                    but all the information that will be displayed here can be accessed 
-                    by following the links below.
+                    This landing page is still under construction, but
+                    <em> all the information that will be displayed here in the future 
+                    can be accessed by following the links below.</em>
                 </p>
             </div>
         </main>
