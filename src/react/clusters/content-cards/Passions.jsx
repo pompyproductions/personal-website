178f3f83cf0bf0420c900d6d38783bfc36e22cdf
Eren Gazioglu <pompyproduction@gmail.com>
2022-11-20 14:29:55 +0000

    Create expanding sections for 'Passions' card

--- src/react/clusters/content-cards/Passions.jsx
+++ src/react/clusters/content-cards/Passions.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Bubbles from '../../canvas/Bubbles.jsx';
+import ExpandSection from '../../components/ExpandSection.jsx';
 import TextCouple from '../../components/TextCouple.jsx';
 
 const Passions = () => {
     return (
         <article className="content-card">
+
+
+
+
             <Bubbles/>
             <h2>Passions</h2>
 
-            <section className="content-bit">
-                <h3>Programming</h3>
+            <ExpandSection
+                title="Programming"
+                className="active"
+            >
                 <p>Self-taught programmer & web developer, with a sensibility toward good and intuitive interfaces.</p>
                 <div className="hsep"></div>
                 <ul>
@@ -17,10 +24,11 @@ const Passions = () => {
                     <li><TextCouple title="Technologies">Git, Node.js, React, Sass, Webpack, Gulp.</TextCouple></li>
                     <li><TextCouple title="Other">Godot (GDScript).</TextCouple></li>
                 </ul>
-            </section>
+            </ExpandSection>
 
-            <section className="content-bit">
-                <h3>Architecture</h3>
+            <ExpandSection
+                title="Architecture"
+            >
                 <p>Architect with 2+ years of professional experience, from Portuguese and Italian schools of architecture. Solid drafting, modelling, design, and communication skills.</p>
                 <div className="hsep"></div>
                 <ul>
@@ -37,17 +45,18 @@ const Passions = () => {
                         BSc. in Architectural Sciences (Politecnico di Milano, IT); MSc. in Architecture (Universidade Autónoma de Lisboa, PT).
                     </TextCouple></li>
                 </ul>
-            </section>
+            </ExpandSection>
 
-            <section className="content-bit">
-                <h3>Music Production</h3>
+            <ExpandSection
+                title="Music Production"
+            >
                 <p>Self-taught composer, multi-instrumentalist, and bedroom producer. Began learning to play guitar in 2007, composing and arranging in 2009, and recording in 2011. Currently working on a solo project named GRANOTURCO.</p>
                 <div className="hsep"></div>
                 <ul>
                     <li><TextCouple title="Software">Reaper, Ableton, Cubase, Guitar Pro.</TextCouple></li>
                     <li><TextCouple title="Instruments">Electric/Acoustic Guitar, Bass, Drums/Percussion, Keyboard.</TextCouple></li>
                 </ul>
-            </section>
+            </ExpandSection>
         </article>
     )
 }
